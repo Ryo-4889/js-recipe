b096187527561b411d0fc5da212df26684d766f8
Ryo-4889 <i320008k@mails.cc.ehime-u.ac.jp>
2023-01-28 20:48:32 +0900

    week3

--- 02_レシピ/02_クイズ（１問）/main.js
+++ 02_レシピ/02_クイズ（１問）/main.js
@@ -13,3 +13,28 @@ choice2.onclick = function() {
 }
 choice3.onclick = function() {
   feedback.textContent = "正解！ガニメデは、木星の第三惑星だよ！"
+  // 正解を判定する
+const choose = function(choiceString) {
+    if (choiceString === "ゴリアテ") {
+      feedback.textContent =
+        "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。"
+    } else if (choiceString === "ゼニガメ") {
+      feedback.textContent =
+        "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。"
+    } else if (choiceString === "ガニメデ") {
+      feedback.textContent = "正解！ガニメデは、木星の第三惑星だよ！"
+    }
+  }
+  
+  choice1.onclick = function() {
+    // ゴリアテを選択
+    choose("ゴリアテ")
+  }
+  choice2.onclick = function() {
+    // ゼニガメを選択
+    choose("ゼニガメ")
+  }
+  choice3.onclick = function() {
+    // ガニメデを選択
+    choose("ガニメデ")
+  }
---
+++ 04_ゼミ/week2/session/main.js
@@ -0,0 +1,6 @@
+// idをもとに要素を取得する。
+const addbutton = document.getElementById("add-button")
+const element = document.getElementById("element1")
+const inputmemo = document.getElementById("memo-input")
+
+addbutton.onclick = function () {}
